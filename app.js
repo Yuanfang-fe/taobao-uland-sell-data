73283946293e0edce773e31e4caf15b7aff9cf30
李元芳 <jun.zheng@ideacome.com>
2021-04-01 20:00:09 +0800

    remove node_modules

--- app.js
+++ app.js
@@ -4,14 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var taobaoRouter = require('./routes/taobao');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,7 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/aitaobao', taobaoRouter);
 
 // catch 404 and forward to error handler
